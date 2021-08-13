5b01520564409385cc7819be70d5b50fefbc88f5
Ankit Aggarwal <ankit.aggarwal02@sap.com>
2021-08-13 10:25:52 +0200

    Added postman files and python script

--- README.md
+++ README.md
@@ -1,20 +1,32 @@
 # Title
 
+SAP EPD Connected Products IoT Device Connectivity Support
+
 ## Description
+This repository contains sample postman collection and environment files in the folder: Generic_postman_env_collection which would allow a user to download the IoT device certificate. This device certificate is allowed for setting up the data ingestion for an equipment.
+
+In addition, it also contains an example python script:data_ingestion_example.py which shows how can one use the downloaded device certificate and use it to ingest the data to IoT.
 
 ## Requirements
 
+[Python](https://www.python.org/) and [Postman](https://www.postman.com/downloads/)
+
 ## Download and Installation
 
+Please check the documentation [here](https://help.sap.com/viewer/DRAFT/7f3d709e785c414497c67bb7b0e4ad5e/Internal/en-US/1548753ffd8148f1aea95c5bc2e12067.html).
+
 ## Known Issues
+No known issues so far
 
 ## How to obtain support
 
-[Create an issue](https://github.com/SAP-samples/<repository-name>/issues) in this repository if you find a bug or have questions about the content.
- 
+[Create an issue](https://github.com/SAP-samples/epd-connected-products-iot-connectivity/issues) in this repository if you find a bug or have questions about the content.
+
 For additional support, [ask a question in SAP Community](https://answers.sap.com/questions/ask.html).
 
 ## Contributing
+
+This project is only updated by SAP employees or only accepting bug reports but no other contributions.
 
 ## License
 Copyright (c) 2021 SAP SE or an SAP affiliate company. All rights reserved. This project is licensed under the Apache Software License, version 2.0 except as noted otherwise in the [LICENSE](LICENSES/Apache-2.0.txt) file.
---
+++ data_ingestion_example.py
@@ -0,0 +1,123 @@
+# prerequisite: pip install paho-mqtt
+# see documentation https://www.eclipse.org/paho/clients/python/docs/
+# run with: python data_ingestion_example.py
+import paho.mqtt.client as mqtt
+import ssl
+import time
+import logging
+import json
+import time
+
+
+logging.basicConfig(format='[{asctime},{msecs:03.0f}] {levelname} {name}.{lineno}| {message}',
+                    datefmt='%H:%M:%S',
+                    level=logging.DEBUG,
+                    style='{')
+LOGGER = logging.getLogger()
+
+# insert here your MQTT host, the device/sensor IDs, the path to device certificate (as part of the
+# IoT service key), and the password for that certificate file
+#
+# Mapping of entity IDs:
+# IoT deviceAlternateId -> AC equipment external object ID
+# IoT sensorAlternateId -> AC model template ID
+# IoT capabilityAlternateId -> AC indicator group ID
+host = "sample.cp.iot.sap"
+deviceAlternateId = "BB61BF0AED"
+sensorAlternateId = "pipe_right"
+capabilityAlternateId = "strains_raw"
+certfile = "PATH/TO/CERTIFICATE.pem"
+# !! Do not put this password into a SCM (e.e., Git)!!
+# Instead, read it from an environment variable at runtime
+certfilePassword = "PASSWORD"
+
+# Example payload - each 'measure' item has to match exactly to the capability generated in IoT!
+# Note the special requirements for transferring the time stamp, with property "_time":
+#  - needs not to be modeled in SAP Asset Central
+#  - content is EPOCH-milliseconds
+def get_payload():
+    millis = int(round(time.time() * 1000))
+    payload = {
+        "measures": [
+            {
+                "_time": millis - 500,
+                "strain01": 100,
+                "strain02": 101,
+                "strain03": 102,
+                "strain04": 103
+            }, {
+                "_time": millis,
+                "strain01": 200,
+                "strain02": 201,
+                "strain03": 202,
+                "strain04": 203
+            }
+        ],
+        "sensorAlternateId": sensorAlternateId,
+        "capabilityAlternateId": capabilityAlternateId
+    }
+    return payload
+
+###############################################################################
+
+return_codes = {
+    0: "Connection successful",
+    1: "Connection refused – incorrect protocol version",
+    2: "Connection refused – invalid client identifier",
+    3: "Connection refused – server unavailable",
+    4: "Connection refused – bad username or password",
+    5: "Connection refused – not authorised",
+}
+
+
+def on_connect(mqttc, obj, flags, rc):
+    LOGGER.info(f"==on_connect== connect return code {str(rc)}: {return_codes[rc]}")
+
+def on_message(mqttc, obj, msg):
+    LOGGER.info("==on_message== " + msg.topic + " " + str(msg.qos) + " " + str(msg.payload))
+
+def on_publish(mqttc, obj, mid):
+    LOGGER.info("==on_publish== Message ID: " + str(mid))
+
+def on_subscribe(mqttc, obj, mid, granted_qos):
+    LOGGER.info("==on_subscribe== " + str(mid) + " " + str(granted_qos))
+
+
+# the client ID is essential here!
+mqttc = mqtt.Client(client_id=deviceAlternateId)
+port = 8883
+mqttc.enable_logger()
+
+mqttc.on_message = on_message
+mqttc.on_connect = on_connect
+mqttc.on_publish = on_publish
+mqttc.on_subscribe = on_subscribe
+
+keepalive = 60
+topic = f"measures/{deviceAlternateId}"
+qos = 2
+LOGGER.debug(f"Connecting to {host} port: {str(port)}, keepalive {keepalive}")
+mqttc.connect(host, port, keepalive)
+
+ssl_context = ssl.create_default_context()
+ssl_context.load_cert_chain(certfile, password=certfilePassword)
+mqttc.tls_set_context(ssl_context)
+
+LOGGER.debug("Loop starting...")
+mqttc.loop_start()
+
+for x in range(0, 10):
+    message = json.dumps(get_payload())
+
+    LOGGER.debug(f"Publishing # {x}: topic {topic}, qos {qos}, message {message}")
+    # here comes now the data transfer:
+    infot = mqttc.publish(topic, message, qos=qos)
+    LOGGER.debug(f"Return code: {infot.rc}")
+
+    infot.wait_for_publish()
+    LOGGER.debug("Publishing DONE")
+
+    time.sleep(1)
+
+mqttc.loop_stop()
+mqttc.disconnect()
